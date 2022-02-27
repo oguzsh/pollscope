b600814233623894d599c46956b06c9de297ed79
Oguzhan Ince <oguzhan824@gmail.com>
2022-02-27 21:47:59 +0300

    init - seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+    User.create(name: Faker::Name.name, email: Faker::Internet.email)
+end
+
+5.times do
+    poll = Poll.create(question: Faker::Lorem.question, user: User.find(rand 1..50))
+    4.times do
+        poll.choices.create(choice_text: Faker::Lorem.sentence(word_count: 3))
+    end
+end
+
+100.times do
+    poll = Poll.find(rand 1..5)
+    Vote.create(user: User.find(rand 1..50), poll: poll, choice: poll.choices.sample)
+end
